64ea38af444cccfb369d6f07f8f43f71e2fe429c
Souvik Kundu <kivtech88@gmail.com>
2019-10-30 19:44:36 +0530

    commit

--- firstapp/firstapp/src/app/app.component.ts
+++ firstapp/firstapp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+// decorator
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: '<h1>This is my first angular app<h1>'
 })
+
 export class AppComponent {
-  title = 'firstapp';
+
 }
--- firstapp/firstapp/src/app/app.module.ts
+++ firstapp/firstapp/src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+// decorator
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    
+    // all modules will be declared here
+    imports: [
+        BrowserModule
+    ],
+
+    // all components will be declared here
+    declarations: [
+        AppComponent
+    ],
+
+    // only first or main component
+    bootstrap: [
+        AppComponent
+    ],
+
+    // all services
+    providers: []
 })
-export class AppModule { }
+
+export class AppModule {
+
+}
